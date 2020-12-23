d0e2463ec6a32894d3ba255e3f7218f137e1a879
tomhealy1 <alfaone156@gmail.com>
2020-12-23 21:42:58 +0000

    Add RF model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('lin_model1.pkl', 'rb'))
+model = pickle.load(open('rf_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
