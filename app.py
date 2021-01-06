b0e472303841bcb2356fdb98265157cab0534dbc
tomhealy1 <alfaone156@gmail.com>
2021-01-06 21:02:55 +0000

    added comments

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 model = pickle.load(open('lin_model2.pkl', 'rb'))
 model1 = pickle.load(open('rf_model.pkl', 'rb'))
 
-
+#flask home route
 @app.route('/')
 def home():
     return render_template('home.html')
-
+#flask route for predictions using post method
 @app.route('/predict',methods=['POST'])
 def predict():
+    #takes a float from our input form and pass it to numpy array pass it to our model.
     int_feature = [float(x) for x in request.form.values()]
     final_feature = [np.array(int_feature)]
     prediction = model.predict(final_feature)
-
+    # round our predict to two places
     output = round(prediction[0], 2)
-
+    # Renders our home.html and our prediction text
     return render_template('home.html', prediction_text="The predicted power output using Linear Regression is {} KWh".format(output))
 
 @app.route('/predictrf',methods=['POST'])
